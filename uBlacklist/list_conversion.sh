de5f6c9da018c244faf9a793688a83b0d7befc81
ubuntu <nkeita1207@gmail.com>
2021-02-10 19:40:51 +0900

    update

--- uBlacklist/list_conversion.sh
+++ uBlacklist/list_conversion.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cat origin.txt | sed -e "s/^/*:\/\//g" | sed -e "s/\$/\/*/g" > uBlacklist.txt
-cat origin.txt | sed -e "s/^/-site:/g" | tr '\n' ' '  | sed -e "s/^/https:\/\/www\.google\.co\.jp\/search?q=%s /g" | sed -e "s/\$/\\n/g" > search_url.txt
+cat origin.txt | sort > list.txt
+cat list.txt | sed -e "s/^/*:\/\//g" | sed -e "s/\$/\/*/g" > uBlacklist.txt
+cat list.txt | sed -e "s/^/-site:/g" | tr '\n' ' '  | sed -e "s/^/https:\/\/www\.google\.co\.jp\/search?q=%s /g" | sed -e "s/\$/\\n/g" > search_url.txt
